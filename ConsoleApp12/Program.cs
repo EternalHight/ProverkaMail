83292ef1be8f0bcd177031d9898d4327ce5a3ded
snakeyes1637@yandex.ru <snakeyes1637@yandex.ru>
2020-07-04 12:21:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ProverkaMail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите email = ");
+            string mail = string.Format(Console.ReadLine());
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(mail);
+            if (match.Success)
+                Console.Write(mail + " верный емаил");
+            else
+                Console.Write(mail + " неверный емеил");
+
+
+            Console.ReadKey();
+        }
+    }
+}
